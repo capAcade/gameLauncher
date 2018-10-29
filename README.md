0de7d9fff9a46f5fd0f2f62ec9c2de235bd6aa56
Sven van Straalen <sven.van.straalen@nl.abnamro.com>
2018-10-29 19:53:50 +0100

    new version gamelauncher

--- README.md
+++
@@ -1,2 +0,0 @@
-# gameLauncher
-This is the main menu that will be able to launch the games
---
+++ app.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const path = require('path')
+const AutoLoad = require('fastify-autoload')
+
+
+var copydir = require('copy-dir');
+ 
+copydir.sync('./node_modules/capmangalaxy/docs', './public/capman');
+
+module.exports = function (fastify, opts, next) {
+  // Place here your custom code!
+  fastify.register(require('fastify-static'), {
+    root: path.join(__dirname, 'public'),
+    prefix: '/', // optional: default '/'
+  })
+
+  // Do not touch the following lines
+
+  // This loads all plugins defined in plugins
+  // those should be support plugins that are reused
+  // through your application
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'plugins'),
+    options: Object.assign({}, opts)
+  })
+
+  // This loads all plugins defined in services
+  // define your routes in one of these
+  fastify.register(AutoLoad, {
+    dir: path.join(__dirname, 'services'),
+    options: Object.assign({}, opts)
+  })
+
+  // Make sure to call next when done
+  next()
+}
