80d610727a325fbe731a5116abe3e47d7739608b
Sven van Straalen <ssgvanstraalen@gmail.com>
2019-07-08 20:56:59 +0200

    changed fastify to express

---
+++ build.js
@@ -0,0 +1,50 @@
+'use strict'
+const path = require('path')
+const args = process.argv.slice(2)
+const distFolder = args[0] || 'dist';
+
+var copydir = require('copy-dir');
+var games = [
+    {
+      from: './src/client',
+      to: `./${distFolder}/public`
+    },
+    {
+      from: './src/server',
+      to: `./${distFolder}`
+    },
+    {
+      from: './node_modules/capmangalaxy/docs',
+      to: `./${distFolder}/public/games/capmangalaxy`
+    },
+    {
+      from: './node_modules/capmancrashingbugs/docs',
+      to: `./${distFolder}/public/games/capmancrashingbugs`
+    },
+    {
+      from: './node_modules/phaser-league/src',
+      to: `./${distFolder}/public/games/phaser-league`
+    },
+    {
+      from: './node_modules/break-down',
+      to: `./${distFolder}/public/games/break-down`
+    },
+    {
+      from: './node_modules/capball',
+      to: `./${distFolder}/public/games/capball`
+    }
+  ];
+
+  console.log('copy files');
+  const promiseMap = games.map(game => {
+    let promise = new Promise(function(resolve){
+      copydir(game.from, game.to, function(err){
+        resolve();
+      });
+    });
+    return promise;
+  });
+
+Promise.all(promiseMap).then(function(values) {
+    console.log('files are copied');
+});
