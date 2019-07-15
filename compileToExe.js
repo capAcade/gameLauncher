296510d1b9cb31065cf04ca4d3626b55a8fa5176
Sven van Straalen <sven.van.straalen@nl.abnamro.com>
2019-07-15 20:17:05 +0200

    removed mac

--- compileToExe.js
+++ compileToExe.js
@@ -6,13 +6,6 @@ compile({
   target:'windows-x64-12.6.0',
   output: './dist/gameLauncher'
 }).then(() => {
-  compile({
-    input: './dist/app.js',
-    resources: ['./dist/public/**/**'],
-    target:'macos-x64-v8.4.0',
-    output: './dist/gameLauncher'
-  }).then(() => {
-    console.log('success')
-  })
+  console.log('success')
 })
 
